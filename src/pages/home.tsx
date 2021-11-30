e3633bbf82182c2c8aac1788c7bd2401e1f1da8f
Hari Chauhan <hari-c@hotmail.co.uk>
2021-11-30 19:58:14 +0000

    Create simplified landing

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,7 +6,7 @@ import Logo from "assets/logo.png";
 export const Home: React.FC = () => {
   return (
     <div className="page home">
-      <GroovyButton
+      {/* <GroovyButton
         onClick={() => {
           console.log("");
         }}
@@ -79,8 +79,39 @@ export const Home: React.FC = () => {
         color={Color.YELLOW}
       >
         About
-      </GroovyButton>
+      </GroovyButton> */}
       <img src={Logo} width={700} className="logo" />
+      <div className="manifesto-container">
+        <h1>Manifesto</h1>
+        <p>
+          SODAA are a collective of dreamers. We think about new models of
+          cooperation and how they connect with physical spaces through online
+          and IRL participation.
+        </p>
+
+        <p>
+          Imagine a music venue funded and operated by its community, where
+          every member is involved in the decision-making process. How would
+          that work? What would that sound like?
+        </p>
+
+        <p>
+          SODAA embraces artists, musicians, DJs, producers, coders,
+          songwriters, promoters, hackers, and partygoers (all others). We think
+          they should communicate, participate, celebrate. SODAA listens to all.{" "}
+        </p>
+
+        <p>
+          We form a community that will develop alternative, decentralised
+          decision making and governance models: new ways of making, showing and
+          listening, new spaces for participation.{" "}
+        </p>
+
+        <p>
+          Jump into our Discord, attend our workshops, panels and parties and be
+          part of creating a sustainable and inclusive music ecology.{" "}
+        </p>
+      </div>
     </div>
   );
 };
